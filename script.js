42872d89a603f3d96982f14c0c2b84a481eeee51
John Wells <molekewl2017@gmail.com>
2020-09-05 00:56:27 -0700

    form validation regExp

--- script.js
+++ script.js
@@ -16,3 +16,40 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', e => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate form
+function validate(nameValue, urlValue) {
+  const expression = /^(ftp|http|https):\/\/[^ "]+$/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert('please submit both fields');
+    return false;
+  }
+  // if (urlValue.match(regex)) {
+  //   alert('match');
+  // }
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Handle Data from form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  console.log(nameValue, urlValue)
+  // validate(nameValue, urlValue);
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
